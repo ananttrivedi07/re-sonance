ae6ef209d2b179da117eecdbc12a512f456bee9b
ananttrivedi07 <anant.trivedi07@gmail.com>
2025-07-24 15:40:58 +0200

    installed llama locally, able to record audio, transcribe it and get a responsed from llama.

--- main.py
+++
@@ -1,5 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-print("OpenAI Key?", os.getenv("OPENAI_API_KEY") is not None)
-print("Twilio SID?", os.getenv("TWILIO_ACCOUNT_SID") is not None)
